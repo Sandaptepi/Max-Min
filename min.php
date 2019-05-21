6e27d5680f76b2817d7aea16d41f7e87e6107495
MAM Yousiang <yousiang@MacBook-Pro-de-MAM.local>
2019-05-21 11:07:59 +0700

    add min

---
+++ min.php
@@ -0,0 +1,14 @@
+<?php
+
+$arr = array(5,10,20,30,40,90,876,678,6767,2222);
+$min = $arr[0];
+
+foreach ($arr as $key => $value) {
+	if ($min<value) {
+		$min=value;
+	}
+}
+echo "MIN value =".$min;
+?>
+
+
